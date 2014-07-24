32a477e063c63687f101335f2e0c75c4701a9255
James Brannon <frontendimac2@frontendimac2s-iMac.local>
2014-07-24 17:44:25 +0100

    some listener management

--- js/directives/GridDirective.js
+++ js/directives/GridDirective.js
@@ -6,26 +6,55 @@ app.directive('ngGrid', ['$window', function($window) {
 			var _memory = {x:0, y:0, w:0, h:0}
 			var _margin = 40;
 			var _open = false;
+			var _transEndEventNames = 'transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd';
 
 			//  Methods
 			var handleEvent = function (e) {
 				var eventType = e.type ? e.type : e.name;
 				switch(eventType) {
 					case 'click':
-
-						var $el = angular.element(e.target);
-						$el.toggleClass('focus');
-						if (!_open) {
-							_open = true;
-							centralizeGridItem($el);
+						if (!_open && $(e.target).hasClass('my-list-item')) {
+							openItem(angular.element(e.target));
 						}
 						else {
-							_open = false;
-							closeGridItem($el);
+							closeItem(angular.element(e.target));
 						}
-
+						break;
+					case 'transitionend':
+					case 'webkitTransitionEnd':
+					case 'oTransitionEnd':
+					case 'otransitionend':
+					case 'MSTransitionEnd':
+						if ($(e.target).hasClass('my-list-item')) {
+							if (!$(e.target).hasClass('focus')) {
+								var $items = elem.find('.my-list-item');
+								$items.removeClass('disable');
+							}
+							elem.off(_transEndEventNames, handleEvent);
+						}
 						break;
 				}
+			};
+			var openItem = function ($el) {
+				var $items = elem.find('.my-list-item');
+				$items.addClass('disable');
+				_open = true;
+				elem.on(_transEndEventNames, handleEvent);
+				$el.addClass('focus');
+				$el.data('data-opening', 'true')
+				centralizeGridItem($el);
+			};
+			var closeItem = function ($el) {
+				var $items = elem.find('.my-list-item ');
+				$el.removeClass('focus');
+				_open = false;
+				$el.css({
+					width: _memory.w,
+					height: _memory.h,
+					top: _memory.y,
+					left: _memory.x
+				});
+				elem.on(_transEndEventNames, handleEvent);
 			};
 			var centralizeGridItem = function ($el) {
 				_memory.x = $el.position().left;
@@ -34,49 +63,16 @@ app.directive('ngGrid', ['$window', function($window) {
 				_memory.h = $el.height();
 				var width = _$window.width() - (_margin * 2);
 				var height = _$window.height() - (_margin * 2);
-
-				$el.width(width);
-				$el.height(height);
-
-				$el.css({top: _margin});
-				$el.css({left: _margin});
-
-			};
-			var closeGridItem = function ($el) {
-				console.log('closeGridItem', $el)
-
-				// $el.css("height", null);
-				// $el.css("width", null);
-
 				$el.css({
-					width: _memory.w,
-					height: _memory.h,
-					top: _memory.y,
-					left: _memory.x
+					width: width,
+					height: height,
+					top: _margin,
+					left: _margin
 				});
-
 			};
-			// var monitorScrollPosition = function () {
-			// 	if (_$window.scrollTop() >= _trigger) {
-			// 		_loading = true;
-			// 		scope.$emit('loadMoreItems');
-			// 		_$loader.show();
-			// 	}
-			// 	else {
-			// 		updateMessage(_offset + ' : ' + _$window.scrollTop());
-			// 		_$loader.hide();
-			// 	}
-			// };
-			// var updateMessage = function (str) {
-
-			// 	_$message.html(str);
-			// };
 
 			//  Listeners
 			elem.on('click', handleEvent);
-			// _$window.on('scroll', handleEvent);
-			// _$window.on('resize', handleEvent);
-			// scope.$on('loadComplete', handleEvent);
 		}
   };
 }]);
