b7b63041c7a7351e76bdca9495ad31541b64f691
James Brannon <frontendimac2@frontendimac2s-iMac.local>
2014-07-25 15:33:59 +0100

    initial grid builder logic added

--- js/directives/GridDirective.js
+++ js/directives/GridDirective.js
@@ -7,32 +7,32 @@ app.directive('ngGrid', ['$window', function($window) {
 			var _margin = 40;
 			var _open = false;
 			var _transEndEventNames = 'transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd';
-console.log('ngGrid');
+			var _items = [];
+			var _rows = 3;
+
 			//  Methods
 			var handleEvent = function (e) {
 				var eventType = e.type ? e.type : e.name;
 				switch(eventType) {
 					case 'click':
-						if (!_open && $(e.target).hasClass('my-list-item')) {
-							openItem(angular.element(e.target));
-						}
-						else {
-							closeItem(angular.element(e.target));
-						}
+						userItemSelect(angular.element(e.target));
 						break;
 					case 'transitionend':
 					case 'webkitTransitionEnd':
 					case 'oTransitionEnd':
 					case 'otransitionend':
 					case 'MSTransitionEnd':
-						if ($(e.target).hasClass('my-list-item')) {
-							if (!$(e.target).hasClass('focus')) {
-								var $items = elem.find('.my-list-item');
-								$items.removeClass('disable');
-							}
-							elem.off(_transEndEventNames, handleEvent);
-						}
+						transtitionManager(angular.element(e.target));
 						break;
+				}
+			};
+			var userItemSelect = function ($el) {
+				var _isValidItem = $el.hasClass('my-list-item');
+				if (!_open && _isValidItem) {
+					openItem($el);
+				}
+				else if (_open && _isValidItem) {
+					closeItem($el);
 				}
 			};
 			var openItem = function ($el) {
@@ -40,16 +40,10 @@ console.log('ngGrid');
 				$items.addClass('disable');
 				_open = true;
 				elem.on(_transEndEventNames, handleEvent);
-
-				console.log('element',$el[0])
-
-
 				$el.addClass('focus');
-				$el.data('data-opening', 'true')
 				centralizeGridItem($el);
 			};
 			var closeItem = function ($el) {
-				var $items = elem.find('.my-list-item ');
 				$el.removeClass('focus');
 				_open = false;
 				$el.css({
@@ -63,17 +57,10 @@ console.log('ngGrid');
 			var centralizeGridItem = function ($el) {
 				_memory.x = $el.position().left;
 				_memory.y = $el.position().top;
-				_memory.w = $el.width();
-				_memory.h = $el.height();
+				_memory.w = $el.outerWidth();
+				_memory.h = $el.outerHeight();
 				var width = _$window.width() - (_margin * 2);
 				var height = _$window.height() - (_margin * 2);
-
-				console.log('width', width);
-				console.log('height', height);
-				console.log('window', _$window[0]);
-
-				elem.append('<p style="position: absolute;bottom: 0">'+height+$window+'</p>')
-
 				$el.css({
 					width: width,
 					height: height,
@@ -81,9 +68,91 @@ console.log('ngGrid');
 					left: _margin
 				});
 			};
+			var transtitionManager = function ($el) {
+				var _isValidItem = $el.hasClass('my-list-item');
+				var $items = elem.find('.my-list-item');
+				if (_isValidItem) {
+					if (!$el.hasClass('focus')) {
+						$items.removeClass('disable');
+					}
+					elem.off(_transEndEventNames, handleEvent);
+				}
+			};
+
+			var gridGlue = function () {
+
+				var height = Math.ceil(_$window.height() * .8);
+				var itemHeight = Math.ceil(height * .33);
+				elem.height(height);
+
+				var $items = elem.find('.my-list-item');
+				var $item = false;
+				var len = $items.length;
+				var inc = 0;
+				var o = {};
+				var twidth = 0;
+
+				while (len--) {
+					$item = $($items[inc]);
+					o = {
+						h: itemHeight,
+						w: itemHeight,
+						top: 0,
+						left: 0,
+						$el: $item
+					}
+					if ($item.hasClass('large')) {
+						o.w = itemHeight * 2;
+					}
+					twidth = twidth + o.w;
+					_items.push(o);
+					inc++;
+				}
+
+				len = _items.length;
+				inc = 0;
+				row = 0;
+				col = 0;
+				var width_limiter = twidth/_rows;
+				var temp_width = 0;
+				var temp_left;
+
+				while (len--) {
+
+					temp_width = temp_width + _items[inc].w;
+					if (temp_width > width_limiter && row < (_rows-1)) {
+						temp_width = _items[inc].w;
+						row++;
+						col = 0;
+					}
+
+					if (col === 0) {
+						temp_left = 0;
+					}
+					else {
+						temp_left = temp_left + _items[inc-1].w;
+					}
+
+					_items[inc].$el.css({
+						height: _items[inc].h,
+						width: _items[inc].w,
+						top: _items[inc].h * row,
+						left: temp_left,
+					});
+
+					col++;
+					inc++;
+
+					if (len === 0) {
+						elem.width(temp_width);
+					}
+
+				};
+			};
 
 			//  Listeners
 			elem.on('click', handleEvent);
+			gridGlue();
 		}
   };
 }]);
